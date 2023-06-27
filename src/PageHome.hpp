b440afdb9abcf5097eb2994ec79424c3aa87cf7e
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-06-27 20:05:23 -0300

    Criando Classe PageHome(menu) que é derivada de Fase

---
+++ src/PageHome.hpp
@@ -0,0 +1,15 @@
+#ifndef PAGEHOME_HPP
+#define PAGEHOME_HPP
+
+#include "ASCII_ENGINE/Fase.hpp"
+
+class PageHome : public Fase{
+    private:
+        /* data */
+    public:
+        PageHome(/* args */);
+        ~PageHome();
+};
+
+
+#endif
